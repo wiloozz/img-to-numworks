2d3584a0da24666afb305b2ae2dd67fd15629ac1
wiloozz <bananecool2@gmail.com>
2025-06-29 07:32:21 +0200

    Update encode.py

--- encode.py
+++ encode.py
@@ -1,38 +1,38 @@
 import parameters
 
-amountPass = 0
-
-newText = ""
-
 with open(parameters.outputFile,"r") as file:
     text = file.read()
 
 def compress_binary_image(binary_string):
-    if not binary_string:
-        return ""
+    
+    result : list = []
+    if binary_string[-1] == "1":
+        binary_string += "0"
+    else:
+        binary_string += "1"
 
-    result = []
-    current_char = binary_string[0]
+    if binary_string[0] == "1":
+        currentChar = "1"
+    else:
+        currentChar = "0"
+        
     count = 1
-
+        
     for bit in binary_string[1:]:
-        if bit == current_char:
+        if bit == currentChar:
             count += 1
         else:
-            if current_char == '1':
-                result.append(f"|{count}|")
-            else:
-                result.append(f"{{{count}}}")
-            current_char = bit
+            result.append(f"{count}|")
             count = 1
-
-    # Append the last run
-    if current_char == '1':
-        result.append(f"|{count}|")
+            if currentChar == "1":
+                currentChar = "0"
+            else:
+                currentChar = "1"
+                              
+    if result[0] == "0":
+        return "0" + ''.join(result)
     else:
-        result.append(f"{{{count}}}")
-
-    return ''.join(result)
+        return ''.join(result)
 
 compressed_output = compress_binary_image(text)
 print(compressed_output)
