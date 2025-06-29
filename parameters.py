ea2190000390a3451e3a1cf6b976f82941357862
wiloozz <bananecool2@gmail.com>
2025-06-29 08:07:30 +0200

    Update parameters.py

--- parameters.py
+++ parameters.py
@@ -1,4 +1,4 @@
-SRCimage : str = "" # // the image you want to convert
+SRCimage : str = "" # // the image you want to convert 🛑 USE "/" AND NOT "\" 🛑
 colorSensibility : float = 382.5 # // the higher the value, the lighter the image will be (base value is 382.5)
 outputFile : str = "" # // the file where the encoded image will be written
 showResult : bool = False # // if turned on, the getImage script will render the image in a kandinsky window 
